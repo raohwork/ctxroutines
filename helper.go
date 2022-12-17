687503e4465fda16f2828a5bacbbaf6c81b88fc8
Ronmi Ren <ronmi.ren@gmail.com>
2022-12-17 16:27:20 +0800

    update CancelOnSignal

--- helper.go
+++ helper.go
@@ -5,22 +5,51 @@
 package ctxroutines
 
 import (
+	"context"
 	"os"
 	"os/signal"
+	"sync"
 )
 
-// CancelOnSigal runs r and calls r.Cancel when receiving first signal in sig
-func CancelOnSignal(r Runner, sig ...os.Signal) (err error) {
+// ErrSignalReceived indicates the signal that SignalRunner received
+type ErrSignalReceived struct {
+	os.Signal
+}
+
+func (s ErrSignalReceived) Error() string {
+	return "signal received: " + s.String()
+}
+
+// SignalRunner creates a Runner that waits first signal in sig and returns it as error
+func SignalRunner(sig ...os.Signal) Runner {
 	ch := make(chan os.Signal)
 	signal.Notify(ch, sig...)
-	go func() {
-		<-ch
+	once := sync.Once{}
+	f := func() {
 		signal.Reset(sig...)
 		close(ch)
-		r.Cancel()
 		for range ch {
 		}
-	}()
+	}
+	return FuncRunner(func() {
+		once.Do(f)
+	}, func() error {
+		s := <-ch
+		once.Do(f)
+		if s == nil {
+			return context.Canceled
+		}
 
-	return r.Run()
+		return ErrSignalReceived{
+			Signal: s,
+		}
+	})
+}
+
+// CancelOnSignal runs r and calls r.Cancel when receiving first signal in sig
+func CancelOnSignal(r Runner, sig ...os.Signal) (err error) {
+	x := Skip(r, SignalRunner(sig...))
+	defer x.Cancel()
+
+	return x.Run()
 }
